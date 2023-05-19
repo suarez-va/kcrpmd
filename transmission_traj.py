56750b355e0370ee7a3f73d3722cd36b0cd45e02
suarez-va <suarez.a.victor@gmail.com>
2023-05-19 10:29:49 -0400

    adding both run.py files

--- transmission_traj.py
+++ transmission_traj.py
@@ -24,16 +24,23 @@ a = 5e-6
 b = 400.
 L = 0.1
 eta = 2 * np.pi
-my = K**2 * beta**3 / (2 * np.pi * L**2)
+my = 10. #K**2 * beta**3 / (2 * np.pi * L**2)
 system = KcrpmdSystem.SystemA(beta, a, b, L, eta, my, sysparam)
 
-dt = 2.5
-Nstep = 40000000
-Nprint = 2500
+dt = 0.05
+Nstep = 200000
+Nprint = 1000
 gammay = 1 / (2 * (2 - L)) * np.sqrt((1 - 2 * np.log((2 - L) / L * np.sqrt(a / np.pi) * eta * beta**2) - 4 * np.log(K)) / (beta * my))
-Ntemp = 25000
+Ntemp = Nstep
 set_y = 0.
-kcrpmd = Kcrpmd(system, dt, Nstep, Nprint, langevin_nve=False, gammay=gammay, resample_vel=True, Ntemp=Ntemp, fix_y=True, set_y=set_y)
+kcrpmd = Kcrpmd(system, dt, Nstep, Nprint, langevin_nve=True, gammay=gammay, resample_vel=False, Ntemp=Ntemp, fix_y=False, set_y=set_y)
+kcrpmd.resample_vy()
+
+positions_dat = open('/storage/home/hcoda1/8/vsuarez6/kcrpmd_5.18.2023/trajectories/traj_aaa/positions.dat', "r")
+yR = np.array(positions_dat.readlines()[10 * bbb + 5000].split(), dtype=np.float64)
+kcrpmd.R = yR[1:]
+kcrpmd.resample_vy()
+kcrpmd.resample_vR()
 
 kcrpmd.kernel()
 
