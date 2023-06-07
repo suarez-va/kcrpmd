7719457bdc701347bfe7de40ccfb3e9e1bba1d55
suarez-va <suarez.a.victor@gmail.com>
2023-06-07 07:28:15 -0400

    added parameters for next calculation

--- kinked_pair_thermalization.py
+++ kinked_pair_thermalization.py
@@ -6,13 +6,14 @@ import kcrpmd_system as KcrpmdSystem
 from kcrpmd import Kcrpmd
 
 epsilon_array = np.array([0., 0.04, 0.07, 0.118, 0.18, 0.236])
+K_array = np.array([6.67e-7, 4.31e-6, 2.78e-5, 1.8e-4, 1.16e-3, 7.5e-3])
 
 ms = 1836.
 omegas = 2.28e-3
 s0 = -2.40
 s1 = 2.40
-epsilon = -epsilon_array[aaa - 1]
-K = 6.67e-7
+epsilon = -epsilon_array[0]
+K = K_array[aaa - 1]
 M = 1836.
 omegac = 2.28e-3
 gamma = 1. * M * omegac
@@ -21,7 +22,7 @@ sysparam = np.array([ms, omegas, s0, s1, epsilon, K, M, omegac, gamma, fmodes])
 
 beta = 1 / (300 * 3.16681156345e-6)
 a = 5e-6
-b = 400.
+b = 4000.
 L = 0.1
 eta = 2 * np.pi
 my = K**2 * beta**3 / (2 * np.pi * L**2)
